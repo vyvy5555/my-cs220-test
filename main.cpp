1de16ee567700ca4639e9de2e6dbc7f9f90bff6c
vyvy5555 <anhvyphamngoc@gmail.com>
2025-11-06 13:05:51 -0800

    string stuff

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main()
 {
     cout << "Hello World" << endl;
     cout << "This is fun!" << endl;
+
+    string s = "Greeting!";
+    cout << s << endl;
 }
